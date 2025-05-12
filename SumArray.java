c70a8978be1a614ed2083499457c346d0a70aa1a
Arijitakayal <168023785+Arijitakayal@users.noreply.github.com>
2025-05-12 10:50:03 -0700

    Add files via upload

---
+++ SumArray.java
@@ -0,0 +1,15 @@
+public class SumArray {
+    public static void main(String[] args) {
+        int[] arr = {4, 8, 12, 16};
+        int sum = 0;
+
+        for (int num : arr) {
+            sum += num;
+        }
+
+        System.out.println("Sum: " + sum);
+    }
+}
+
+    
+
